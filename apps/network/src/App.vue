bee9b51261486d4bf9814fd2c6c5f8dc751a71a6
Juan de Dios Martínez Vallejo <vallemardeveloper@gmail.com>
2024-02-23 00:45:20 +0100

    network: show response body

--- apps/network/src/App.vue
+++ apps/network/src/App.vue
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 import { ref } from "vue";
 import { CDP, RequestWillBeSent, ResponseReceived, GetResponseBodyRequest, GetResponseBodyRespond, LoadingFinished } from "./types"
 const socket = io("http://localhost:3000");
-const data = ref<Map<string, { requestWillBeSent: RequestWillBeSent, responseReceived?: ResponseReceived, finish?: LoadingFinished, body?: GetResponseBodyRespond }>>(new Map())
+const data = ref<Map<string, { requestWillBeSent: RequestWillBeSent, responseReceived?: ResponseReceived, finish?: LoadingFinished, body?: CDP<GetResponseBodyRespond>, open?: boolean }>>(new Map())
 let lastRequestId = "0";
 socket.on("cdp", (message: any) => {
   if (message.method == "Network.requestWillBeSent") {
@@ -33,28 +33,39 @@ socket.on("cdp", (message: any) => {
     }
   }
 
-  if (message.result && message.result.body && message.result.base64Encoded) {
+  if (message.result && message.result.body) {
     const dataCDP = message as CDP<GetResponseBodyRespond>;
     const request = data.value.get(lastRequestId)
     if (request) {
-      request.body = dataCDP.result;
+      request.body = dataCDP;
       data.value.set(lastRequestId, request)
     }
   }
 });
+
+function open(requestId: string) {
+  const request = data.value.get(requestId)!;
+  request.open = !request?.open;
+}
 </script>
 
 <template>
-  <div class="w-full h-full text-left pl-4 table">
+  <div class="w-full h-full text-left pl-4 block">
     <div class="header py-2">
       <div class="col">Name</div>
       <div class="col">Method</div>
       <div class="col">Status</div>
     </div>
-    <div v-for="(request, i) in data.keys()" :key="i" class="w-full cursor-pointer row py-1">
-      <td class="text-left col">{{ data.get(request)?.requestWillBeSent?.request.url }}</td>
-      <td class="col"> {{ data.get(request)?.requestWillBeSent?.request.method }}</td>
-      <td class="col">{{ data.get(request)?.responseReceived?.response.status }}</td>
+    <div v-for="(request, i) in data.keys()" :key="i" class="w-full cursor-pointer row py-1" @click="open(request)">
+      <div class="text-left col">{{ data.get(request)?.requestWillBeSent?.request.url }}</div>
+      <div class="col"> {{ data.get(request)?.requestWillBeSent?.request.method }}</div>
+      <div class="col">{{ data.get(request)?.responseReceived?.response.status }}</div>
+      <div class="details w-full overflow-x-auto p-4" v-if="data.get(request)?.open"
+        @click="($event) => $event.stopPropagation()">
+        <div class="text-xl font-bold">Response</div>
+        <span>{{ data.get(request)?.body?.result?.body }}
+        </span>
+      </div>
     </div>
     <div v-if="data.size === 0" class="mt-4 text-center">No request captured</div>
   </div>
@@ -67,17 +78,12 @@ socket.on("cdp", (message: any) => {
 .header,
 .row {
   display: flex;
-  /* aligns all child elements (flex items) in a row */
+  flex-wrap: wrap;
 }
 
 .col {
   flex: 1;
   padding: 2px 8px;
-  /* distributes space on the line equally among items */
 }
-
-/* .col:nth-child(even) {
-  background-color: #182235;
-} */
 </style>
 
